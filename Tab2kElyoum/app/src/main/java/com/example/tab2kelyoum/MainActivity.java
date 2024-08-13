47f183e9bbcc160af07c14c15092fc9935d0cfe0
rehamelsayedd294 <rehamelsayeddd@yahoo.com>
2024-08-13 14:26:30 +0300

    implementing the splash screen

--- Tab2kElyoum/app/src/main/java/com/example/tab2kelyoum/MainActivity.java
+++ Tab2kElyoum/app/src/main/java/com/example/tab2kelyoum/MainActivity.java
@@ -7,6 +7,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,6 +17,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
+        //adding nav controller
+        NavController navController = Navigation.findNavController(this ,R.id.nav_host_fragment);
 
     }
 }
