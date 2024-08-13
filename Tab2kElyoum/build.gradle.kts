13098473b395a2ffed811b5e9835a2b274cc64c3
rehamelsayedd294 <rehamelsayeddd@yahoo.com>
2024-08-13 14:12:45 +0300

    adding the implementation to gradle file

--- Tab2kElyoum/build.gradle.kts
+++ Tab2kElyoum/build.gradle.kts
@@ -1,3 +1,13 @@
+buildscript {
+
+    dependencies {
+        classpath ("androidx.navigation:navigation-safe-args-gradle-plugin:2.5.3")
+
+        //for google sign in with firebase
+        classpath ("com.google.gms:google-services:4.3.14")
+    }
+}
+
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
