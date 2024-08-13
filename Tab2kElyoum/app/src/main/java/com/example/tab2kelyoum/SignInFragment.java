d7266f0968b3bc270546361080488ba20343f014
rehamelsayedd294 <rehamelsayeddd@yahoo.com>
2024-08-13 16:34:33 +0300

    register screen created

--- Tab2kElyoum/app/src/main/java/com/example/tab2kelyoum/SignInFragment.java
+++ Tab2kElyoum/app/src/main/java/com/example/tab2kelyoum/SignInFragment.java
@@ -7,9 +7,15 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 
 public class SignInFragment extends Fragment {
+    private TextInputEditText et_email;
+    private TextInputEditText et_password;
+    private Button btnregister;
 
 
 
