9ea07fda9b62b3f35e3945b84094c089bb8eb816
rehamelsayedd294 <rehamelsayeddd@yahoo.com>
2024-08-16 00:40:03 +0300

    edititng api

--- Tab2kElyoum/app/src/main/java/com/example/tab2kelyoum/Connection/API.java
+++ Tab2kElyoum/app/src/main/java/com/example/tab2kelyoum/Connection/API.java
@@ -16,34 +16,34 @@ public interface API {
         String BASE_URL_MealItems = "https://www.themealdb.com/api/json/v1/1/";
 
         @GET("random.php")
-        io.reactivex.rxjava3.core.Observable<RootMeal> getRootRandom();
+        Observable<RootMeal> getRootRandom();
 
         @GET("search.php")
-        io.reactivex.rxjava3.core.Observable<RootSingleMeal> getRootSingleMeal(@Query("s") String mealName);
+        Observable<RootSingleMeal> getRootSingleMeal(@Query("s") String mealName);
 
         @GET("search.php")
-        io.reactivex.rxjava3.core.Observable<RootMealsFromSingleLetter> getRootMealsBySingleLetter(@Query("f") String firstLetterOfMeal);
+        Observable<RootMealsFromSingleLetter> getRootMealsBySingleLetter(@Query("f") String firstLetterOfMeal);
 
         @GET("list.php?a=list")
-        io.reactivex.rxjava3.core.Observable<RootAreasList> getRootAreasList();
+        Observable<RootAreasList> getRootAreasList();
 
         @GET("list.php?c=list")
-        io.reactivex.rxjava3.core.Observable<RootCategoriesList> getRootCategoriesList();
+        Observable<RootCategoriesList> getRootCategoriesList();
 
         @GET("list.php?i=list")
-        io.reactivex.rxjava3.core.Observable<RootIngredientsList> getRootIngredientsList();
+        Observable<RootIngredientsList> getRootIngredientsList();
 
         @GET("filter.php")
-        io.reactivex.rxjava3.core.Observable<RootMeal> getMealsOfSelectedArea(@Query("a") String areaSelected);
+        Observable<RootMeal> getMealsOfSelectedArea(@Query("a") String areaSelected);
 
         @GET("filter.php")
-        io.reactivex.rxjava3.core.Observable<RootMeal> getMealsOfSelectedCategory(@Query("c") String categorySelected);
+        Observable<RootMeal> getMealsOfSelectedCategory(@Query("c") String categorySelected);
 
         @GET("filter.php")
-        io.reactivex.rxjava3.core.Observable<RootMeal> getMealsOfSelectedIngredient(@Query("i") String ingredientSelected);
+        Observable<RootMeal> getMealsOfSelectedIngredient(@Query("i") String ingredientSelected);
 
         @GET("lookup.php")
-        io.reactivex.rxjava3.core.Observable<RootMeal> getMealById(@Query("i") int parseInt);
+        Observable<RootMeal> getMealById(@Query("i") int parseInt);
 
         @GET("search.php")
         Observable<RootSingleMeal> getAllMeal(@Query("f") String mealName);
